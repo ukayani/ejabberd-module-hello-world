91e41d89619b6b3a828aefb73f8d9a0a784b205a
Umair Kayani <umair@500px.com>
2018-06-12 17:11:51 -0400

    - added roster retrieval

--- src/mod_helloworld.erl
+++ src/mod_helloworld.erl
@@ -6,9 +6,10 @@
 -include("ejabberd.hrl").
 -include("xmpp.hrl").
 -include("logger.hrl").
+-include("mod_roster.hrl").
 
 %% gen_mod API callbacks
--export([start/2, stop/1, depends/2, mod_opt_type/1, reload/3, in_subscription/6]).
+-export([start/2, stop/1, depends/2, mod_opt_type/1, reload/3, in_subscription/6, get_roster/2]).
 -define(EMPTY_URL, <<"">>).
 
 start(Host, Opts) ->
@@ -28,15 +29,17 @@ in_subscription(_, User, Server, JID, Type, Reason) ->
     process_subscription(in, User, Server, JID, Type,
         Reason).
 
-process_subscription(_, User, _, JID1,
+process_subscription(_, User, Server, JID1,
     subscribe, _) ->
 
     SUser = binary_to_list(User),
     SJID = format_jid(JID1),
 
+    Items = get_roster(User, Server),
     ?INFO_MSG("--------------------------------------------", []),
     ?INFO_MSG("--- Subscription Initiated -----------------", []),
     ?INFO_MSG("~p recieved a subscription request from ~p", [SUser, SJID]),
+    ?INFO_MSG("~p", [Items]),
     ?INFO_MSG("--------------------------------------------", []);
 
 process_subscription(_, User, _Server, JID1,
@@ -57,6 +60,28 @@ process_subscription(_Direction, _User, _Server, _JID1,
     ?INFO_MSG("Processing unsubscribe(d)", []),
     ?INFO_MSG("--------------------------------------------", []).
 
+get_roster(User, Server) ->
+  Items = ejabberd_hooks:run_fold(roster_get, Server, [], [{User, Server}]),
+  make_roster_xmlrpc(Items).
+
+%% Note: if a contact is in several groups, the contact is returned
+%% several times, each one in a different group.
+make_roster_xmlrpc(Roster) ->
+  lists:foldl(
+    fun(Item, Res) ->
+      JIDS = jid:encode(Item#roster.jid),
+      Nick = Item#roster.name,
+      Subs = atom_to_list(Item#roster.subscription),
+      Ask = atom_to_list(Item#roster.ask),
+      Groups = case Item#roster.groups of
+                 [] -> [<<>>];
+                 Gs -> Gs
+               end,
+      ItemsX = [{JIDS, Nick, Subs, Ask, Group} || Group <- Groups],
+      ItemsX ++ Res
+    end,
+    [],
+    Roster).
 
 format_jid(JID) ->
     binary_to_list(jid:to_string(JID)).
